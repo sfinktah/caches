01e650568c4578d2f575d4fe1e5d17f6060abc6a
Sfinktah Bungholio <sfinktah@github.spamtrak.org>
2018-01-12 11:20:25 +1100

    added a bunch of extra methods for my convenience

--- include/cache.hpp
+++ include/cache.hpp
@@ -10,85 +10,82 @@
 
 namespace caches {
 
-// Base class for caching algorithms
-template <typename Key, typename Value, typename Policy = NoCachePolicy<Key>>
-class fixed_sized_cache {
- public:
-  using iterator = typename std::unordered_map<Key, Value>::iterator;
-  using const_iterator =
-      typename std::unordered_map<Key, Value>::const_iterator;
-  using operation_guard = typename std::lock_guard<std::mutex>;
+    // Base class for caching algorithms
+    template <typename Key, typename Value, typename Policy = NoCachePolicy<Key>>
+    class fixed_sized_cache {
+    public:
+        using iterator        = typename std::unordered_map<Key, Value>::iterator;
+        using const_iterator  = typename std::unordered_map<Key, Value>::const_iterator;
+        using operation_guard = typename std::lock_guard<std::mutex>;
 
-  fixed_sized_cache(size_t max_size, const Policy& policy = Policy())
-      : max_cache_size{max_size}, cache_policy(policy) {
-    if (max_cache_size == 0) {
-      max_cache_size = std::numeric_limits<size_t>::max();
-    }
-  }
+        fixed_sized_cache(size_t max_size, const Policy& policy = Policy()) : max_cache_size{max_size}, cache_policy(policy) {
+            if (max_cache_size == 0)
+                max_cache_size = std::numeric_limits<size_t>::max();
+        }
 
-  void Put(const Key& key, const Value& value) {
-    operation_guard{safe_op};
-    auto elem_it = FindElem(key);
+        void Put(const Key& key, const Value& value) {
+            operation_guard{safe_op};
+            auto elem_it = FindElem(key);
 
-    if (elem_it == cache_items_map.end()) {
-      // add new element to the cache
-      if (Size() + 1 > max_cache_size) {
-        auto disp_candidate_key = cache_policy.ReplCandidate();
+            if (elem_it == cache_items_map.end()) {
+                // add new element to the cache
+                if (Size() + 1 > max_cache_size) {
+                    auto disp_candidate_key = cache_policy.ReplCandidate();
 
-        Erase(disp_candidate_key);
-      }
+                    Erase(disp_candidate_key);
+                }
 
-      Insert(key, value);
-    } else {
-      // update previous value
-      Update(key, value);
-    }
-  }
+                Insert(key, value);
+            }
+            else {
+                // update previous value
+                Update(key, value);
+            }
+        }
 
-  const Value& Get(const Key& key) const {
-    operation_guard{safe_op};
-    auto elem_it = FindElem(key);
+        const Value& Get(const Key& key) const {
+            operation_guard{safe_op};
+            auto elem_it = FindElem(key);
 
-    if (elem_it == cache_items_map.end()) {
-      throw std::range_error{"No such element in the cache"};
-    }
-    cache_policy.Touch(key);
+            if (elem_it == cache_items_map.end()) {
+                throw std::range_error{"No such element in the cache"};
+            }
 
-    return elem_it->second;
-  }
+            cache_policy.Touch(key);
 
-  const size_t Size() const {
-    operation_guard{safe_op};
+            return elem_it->second;
+        }
 
-    return cache_items_map.size();
-  }
+        const size_t Size() const {
+            operation_guard{safe_op};
 
- protected:
-  void Insert(const Key& key, const Value& value) {
-    cache_policy.Insert(key);
-    cache_items_map.emplace(std::make_pair(key, value));
-  }
+            return cache_items_map.size();
+        }
 
-  void Erase(const Key& key) {
-    cache_policy.Erase(key);
-    cache_items_map.erase(key);
-  }
+    protected:
+        void Insert(const Key& key, const Value& value) {
+            cache_policy.Insert(key);
+            cache_items_map.emplace(std::make_pair(key, value));
+        }
 
-  void Update(const Key& key, const Value& value) {
-    cache_policy.Touch(key);
-    cache_items_map[key] = value;
-  }
+        void Erase(const Key& key) {
+            cache_policy.Erase(key);
+            cache_items_map.erase(key);
+        }
 
-  const_iterator FindElem(const Key& key) const {
-    return cache_items_map.find(key);
-  }
+        void Update(const Key& key, const Value& value) {
+            cache_policy.Touch(key);
+            cache_items_map[key] = value;
+        }
 
- private:
-  std::unordered_map<Key, Value> cache_items_map;
-  mutable Policy cache_policy;
-  mutable std::mutex safe_op;
-  size_t max_cache_size;
-};
+        const_iterator FindElem(const Key& key) const { return cache_items_map.find(key); }
+
+    private:
+        std::unordered_map<Key, Value> cache_items_map;
+        mutable Policy     cache_policy;
+        mutable std::mutex safe_op;
+        size_t             max_cache_size;
+    };
 }
 
 #endif  // CACHE_HPP
--- include/fifo_cache_policy.hpp
+++ include/fifo_cache_policy.hpp
@@ -5,31 +5,26 @@
 #include "cache_policy.hpp"
 
 namespace caches {
-template <typename Key>
-class FIFOCachePolicy : public ICachePolicy<Key> {
- public:
-  FIFOCachePolicy() = default;
-  ~FIFOCachePolicy() = default;
+    template <typename Key>
+    class FIFOCachePolicy : public ICachePolicy<Key> {
+    public:
+        FIFOCachePolicy()  = default;
+        ~FIFOCachePolicy() = default;
 
-  void Insert(const Key& key) override {
-    fifo_queue.emplace_front(key);
-  }
-  // handle request to the key-element in a cache
-  void Touch(const Key& key) override {
-    // nothing to do here in the FIFO strategy
-  }
-  // handle element deletion from a cache
-  void Erase(const Key& key) override {
-    fifo_queue.pop_back();
-  }
+        void Insert(const Key& key) override { fifo_queue.emplace_front(key); }
+        // handle request to the key-element in a cache
+        void Touch(const Key& key) override {
+            // nothing to do here in the FIFO strategy
+        }
+        // handle element deletion from a cache
+        void Erase(const Key& key) override { fifo_queue.pop_back(); }
 
-  // return a key of a replacement candidate
-  const Key& ReplCandidate() const override {
-    return fifo_queue.back();
-  }
- private:
-  std::list<Key> fifo_queue;
-};
-} // namespace caches
+        // return a key of a replacement candidate
+        const Key& ReplCandidate() const override { return fifo_queue.back(); }
 
-#endif // FIFO_CACHE_POLICY_HPP
+    private:
+        std::list<Key> fifo_queue;
+    };
+}  // namespace caches
+
+#endif  // FIFO_CACHE_POLICY_HPP
